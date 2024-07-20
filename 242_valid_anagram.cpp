3f5c8c4b3903ab91b285d8dbe69a75f4a9f8c2cb
Mohit Yadav <mohitydv09@gmail.com>
2024-07-20 13:12:09 -0500

    242 with map

--- 242_valid_anagram.cpp
+++ 242_valid_anagram.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s == t){
-            return true;
+        if(s.size() != t.size()){
+            return false;
         }
-        return false;
+        map<char, int> s_count;
+        map<char, int> t_count;
+        for (int i = 0; i < s.size(); i++){
+            s_count[s[i]]++;
+            t_count[t[i]]++;
+        }
+        for (const auto& pair : s_count){
+            if(s_count[pair.first] != t_count[pair.first]){
+                return false;
+            }
+        }
+        return true;
     }
 };
 
