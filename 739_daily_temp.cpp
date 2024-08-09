149f5a7e9407c2e1a88a4858157551ba43208724
Mohit Yadav <mohitydv09@gmail.com>
2024-08-09 13:35:11 -0500

    739 updated

--- 739_daily_temp.cpp
+++ 739_daily_temp.cpp
@@ -6,40 +6,30 @@ using namespace std;
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> ans(temperatures.size(),0);
-        stack<pair<int, int>> monoStack; // first is temp, second in index.
-
-        // keep the first item in stack.
-        monoStack.push({temperatures[0], 0});
-
-        for (int i = 1; i < temperatures.size(); i++){
-            // if the current temp is greater that top of stack that means we found the next high temp.
-            auto topStack = monoStack.top();
-            while(topStack.first < temperatures[i]){
-                ans[topStack.second] = i - topStack.second;
-                monoStack.pop();
-                if(!monoStack.empty()){
-                    topStack = monoStack.top();
-                }else{
-                    break;
-                }
+        vector<int> answer(temperatures.size(), 0);
+        stack<int> stk;
+        //Push the first elements index.
+        stk.push(0);
+        for (int i = 1; i < temperatures.size(); ++i){
+            while(stk.size() > 0 && temperatures[stk.top()] < temperatures[i]){
+                answer[stk.top()] = i - stk.top();
+                stk.pop();
             }
-            // Add the curr item to top of stack
-            monoStack.push({temperatures[i],i});
+            stk.push(i);
         }
-
-        cout << "Ans : ";
-        for(int item : ans){
-            cout << item << " ";
+        cout << "answer: " << endl;
+        for (int item : answer){
+            cout << item << " " << endl;
         }
-        cout << endl;
-        return ans;
+        return answer;
     }
 };
 
+// answer = [1,1,4,2,1,1,0,0]
+
 int main(){
-    Solution obj;
+    Solution solution;
     vector<int> temperatures = {73,74,75,71,69,72,76,73};
-    vector<int> ans = obj.dailyTemperatures(temperatures);
+    vector<int> ans = solution.dailyTemperatures(temperatures);
     return 0;
 }
