99e417dd65af1e03c57778df2cc9a6a4832724fd
Mohit Yadav <mohitydv09@gmail.com>
2024-09-17 09:41:42 -0500

    124 added

---
+++ 124_bt_max_path_sum.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxSum = INT_MIN;
+    int maxPathSum(TreeNode* root) {
+        int _ = dfs(root);
+        return maxSum;
+    }
+
+private:
+    int dfs(TreeNode* root){
+        if(!root){return 0;}
+
+        int leftMax = std::max(dfs(root->left), 0);
+        int rightMax = std::max(dfs(root->right), 0);
+
+        maxSum = std::max(maxSum, root->val + leftMax + rightMax);
+        return root->val + std::max(leftMax, rightMax);
+    }
+};
+
+int main() {
+    std::vector<int> input = {4,2,7,1,3,6,9};
+
+    // Create Tree from Input.
+    TreeNode* root = new TreeNode(input[0]);
+    std::queue<TreeNode*> q;
+    q.push(root);
+
+    for (int i = 1; i < input.size(); ++i){
+        TreeNode* current = q.front();
+        TreeNode* newNode = new TreeNode(input[i]);
+        if(!current->left){
+            current->left = newNode;
+        }else if(!current->right){
+            current->right = newNode;
+            q.pop();
+        }
+        q.push(newNode);
+    }
+
+    Solution solution;
+    int answer = solution.maxPathSum(root);
+    std::cout << "Answer: " << answer << std::endl;
+};
