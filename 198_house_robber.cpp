e8f2eb19f316ecd581e746c4577b2caf0e0cdf2a
Mohit <mohitydv09@gmail.com>
2024-10-24 17:52:08 -0500

    198 updated

--- 198_house_robber.cpp
+++ 198_house_robber.cpp
@@ -1,30 +1,24 @@
 #include<iostream>
-
-using namespace std;
+#include<vector>
 
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
+    int rob(std::vector<int>& nums) {
+        if(nums.size()==1){return nums[0];}
+
+        int secondLast = nums[0];
+        int last = std::max(nums[0], nums[1]);
+        for(int i = 2; i < nums.size(); ++i){
+            int temp = last;
+            last = std::max(nums[i] + secondLast, last);
+            secondLast = temp;
         }
-        vector<int> max_loot(nums.size(),0);
-        max_loot[0] = nums[0];
-        max_loot[1] = max(nums[0], nums[1]);
-        for (int i = 2; i < nums.size(); i++){
-            max_loot[i] = max(max_loot[i-1], max_loot[i-2] + nums[i]);
-        }
-        cout << "Max loot";
-        for(int loot : max_loot){
-            cout << loot << " ";
-        }
-        cout << endl;
-        return 0;
+        return last;
     }
 };
 
 int main(){
     Solution sol_instance;
-    vector<int> nums = {1,2,3,1};
-    cout << sol_instance.rob(nums) << endl;
+    std::vector<int> nums = {1,2,3,1};
+    std::cout << sol_instance.rob(nums) << std::endl;
 }
