5c3997756e6988ba409a67a15d917256d80b897a
Mohit <mohitydv09@gmail.com>
2024-11-08 11:12:48 -0600

    72 added

--- 72_edit_distance.cpp
+++ 72_edit_distance.cpp
@@ -1,51 +1,44 @@
 #include<iostream>
-
-using namespace std;
+#include<string>
+#include<vector>
 
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        if(word1.size() == 0 && word2.size() == 0){
-            return 0;
-        }else if (word1.size()==0){
-            return word2.size();
-        }else if (word2.size()==0){
-            return word1.size();
+    int minDistance(std::string word1, std::string word2) {
+        std::vector<std::vector<int>> dp(word1.size()+1,std::vector<int>(word2.size()+1,0));
+        for(int i = 0; i < dp.size(); ++i){
+            dp[i][0] = i;
         }
-        vector<vector<int>> grid(word1.size()+1,vector<int>(word2.size()+1,0));
-        for(int i = 0; i < grid.size(); i++){
-            grid[i][0] = i;
+        for(int j = 0; j < dp[0].size(); ++j){
+            dp[0][j] = j;
         }
-        for (int j = 0; j < grid[0].size(); j++){
-            grid[0][j] = j;
-        }
-        for(int i = 1; i < grid.size(); i++){
-            for(int j = 1; j < grid[0].size(); j++){
+
+        for(int i = 1; i < dp.size(); ++i){
+            for(int j = 1; j < dp[0].size(); ++j){
                 if(word1[i-1] != word2[j-1]){
-                    grid[i][j] = 1 + min({grid[i-1][j], grid[i][j-1], grid[i-1][j-1]});
+                    dp[i][j] = 1 + std::min(dp[i-1][j] , std::min(dp[i][j-1], dp[i-1][j-1]));
                 }else{
-                    grid[i][j] = grid[i-1][j-1];
+                    dp[i][j] = dp[i-1][j-1];
                 }
             }
 
         }
-        cout << "Grid : " << endl;
-        for (vector<int> row : grid){
+        std::cout << "DP : " << std::endl;
+        for (auto row : dp){
             for(int item : row){
-                cout << item << " ";
+                std::cout << item << " ";
             }
-            cout << endl;
+            std::cout << std::endl;
         }
-        return grid[grid.size()-1][grid[0].size()-1];
+        return dp[dp.size()-1][dp[0].size()-1];
     }
 };
 
 int main(){
     Solution solution_instance;
     // string word1 = "horse";
-    string word1 = "zoologicoarchaeologist";
-    // string word2 = "ros"; 
-    string word2 = "zoogeologist";
-
-    cout << "Ans : " << solution_instance.minDistance(word1, word2) << endl;
+    // string word1 = "zoologicoarchaeologist";
+    // ststring word2 = "ros"; 
+    // string word2 = "zoogeologist";
+    std::cout << "Answer: " << solution_instance.minDistance("horse", "ros") << std::endl;
 }
