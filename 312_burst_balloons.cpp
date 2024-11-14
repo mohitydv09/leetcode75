68a240960d525333081f616fa369dbab76082765
Mohit <mohitydv09@gmail.com>
2024-11-13 21:12:03 -0600

    10 added

--- 312_burst_balloons.cpp
+++ 312_burst_balloons.cpp
@@ -29,30 +29,3 @@ int main() {
     std::vector<int> nums = {3,1,5,8};
     std::cout << "Answer: " << solution.maxCoins(nums) << std::endl;
 }
-
-// using namespace std;
-
-// int maxCoins(vector<int>& nums) {
-//     // Add virtual balloons with value 1 at both ends
-//     int n = nums.size();
-//     vector<int> extended_nums(n + 2, 1);
-//     for (int i = 0; i < n; ++i) {
-//         extended_nums[i + 1] = nums[i];
-//     }
-
-//     // Initialize DP table
-//     vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
-
-//     // Fill DP table
-//     for (int length = 2; length <= n + 1; ++length) {  // Subarray lengths (from 2 to n+1)
-//         for (int left = 0; left <= n + 1 - length; ++left) {  // Starting index
-//             int right = left + length;
-//             for (int k = left + 1; k < right; ++k) {  // Position of the balloon to burst last
-//                 dp[left][right] = max(dp[left][right], 
-//                                       dp[left][k] + dp[k][right] + extended_nums[left] * extended_nums[k] * extended_nums[right]);
-//             }
-//         }
-//     }
-
-//     // The answer is the maximum coins for the full range
-//     return dp[0][n + 1];
