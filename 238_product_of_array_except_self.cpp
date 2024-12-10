987a1aa13fa3e57f1e16d0074dd22a5fa8a4e68d
Mohit <mohitydv09@gmail.com>
2024-12-10 12:22:04 -0600

    238 updated

--- 238_product_of_array_except_self.cpp
+++ 238_product_of_array_except_self.cpp
@@ -2,37 +2,18 @@
 #include <string>
 #include <vector>
 #include <sstream>
-using namespace std;
 
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        //  Make the forward pass vector.
-        vector<int> forward_pass(nums.size(),1);
-        int prod = 1;
-        for (int i = 1 ; i < nums.size() ; i++){
-            // cout << nums[i] << endl;
-            prod = prod*nums[i-1];
-            forward_pass[i]=prod;
+    std::vector<int> productExceptSelf(std::vector<int>& nums) {
+        std::vector<int> answer(nums.size(),1);
+        for(int i = 1; i < nums.size(); ++i){
+            answer[i] = answer[i-1]*nums[i-1];
         }
-
-        // Make the backword pass vector.
-        vector<int> backward_pass(nums.size(),1);
-        prod = 1;
-        for (int i = nums.size()-2; i >= 0 ; i--){
-            prod = prod*nums[i+1];
-            backward_pass[i]=prod;
-        }
-
-        // Make the product vector.
-        vector<int> answer(nums.size());
-        for (int i = 0; i < nums.size(); i++){
-            answer[i] = forward_pass[i] * backward_pass[i];
-        }
-
-        // Print Answer.
-        for (int item : answer){
-            cout << item << endl;
+        int backProd = 1;
+        for(int i = nums.size()-2; i >= 0; --i){
+            backProd *= nums[i+1];
+            answer[i] *= backProd;
         }
         return answer;
     }
@@ -40,7 +21,7 @@ public:
 
 int main(){
     Solution solution_instance;
-    vector<int> nums = {-1,1,0,-3,3};
-    vector<int> answer = solution_instance.productExceptSelf(nums);
+    std::vector<int> nums = {-1};
+    std::vector<int> answer = solution_instance.productExceptSelf(nums);
     return 0;
 }
