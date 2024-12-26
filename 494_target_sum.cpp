293e88c7cd53d43522de267e20a5f9308d628021
Mohit <mohitydv09@gmail.com>
2024-12-26 16:00:46 -0600

    494 updated

--- 494_target_sum.cpp
+++ 494_target_sum.cpp
@@ -6,26 +6,32 @@ class Solution {
 public:
     int findTargetSumWays(std::vector<int>& nums, int target) {
         int totalSum = std::accumulate(nums.begin(), nums.end(), 0);
-
-        if ((target + totalSum) % 2 != 0 || totalSum + target < 0){
+        if(std::abs(target) > std::abs(totalSum)){
             return 0;
         }
-        int newTarget = (target + totalSum) / 2;
 
-        std::vector<int> dp(newTarget + 1, 0);
-        dp[0] = 1;
-
-        for (auto num : nums){
-            for (int j = newTarget; j > num-1; --j){
-                dp[j] += dp[j - num];
+        //Initialize DP
+        std::vector<int> prev(2*totalSum + 1, 0);
+        prev[totalSum] = 1;
+        for(const int& num : nums){
+            std::vector<int> dp(2*totalSum + 1, 0);
+            for(int i = 0; i < dp.size(); ++i){
+                if(i-num >= 0){
+                    dp[i] += prev[i - num];
+                }
+                if(i + num < dp.size()){
+                    dp[i] += prev[i + num];
+                }                
             }
+            prev = dp;
         }
-        return dp[newTarget];
+        return prev[totalSum + target];
     }
 };
 
+
 int main() {
     Solution solution;
-    std::vector<int> nums = {1,1,1,1,1};
-    std::cout << "Answer: " << solution.findTargetSumWays(nums, 3) << std::endl;
+    std::vector<int> nums = {1};
+    std::cout << "Answer: " << solution.findTargetSumWays(nums, 2) << std::endl;
 }
